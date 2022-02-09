d190444338230229115a8e6e674a0316e9b327b9
devshah00 <54751446+devshah00@users.noreply.github.com>
2022-02-09 15:01:09 -0500

    Added ADC readings

--- hello_world_main.c
+++ hello_world_main.c
@@ -1,28 +1,18 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
 #include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
 
-#define GPIO_OUTPUT_IO_0 GPIO_NUM_26
-#define GPIO_OUTPUT_IO_1 GPIO_NUM_25
-#define GPIO_OUTPUT_PIN_SEL ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
-#define GPIO_INPUT_IO_0 GPIO_NUM_4
-#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_IO_0)
 #define ESP_INTR_FLAG_DEFAULT 0
+#define V_REF 1100
+
+
+// Interrupt - https://www.youtube.com/watch?v=VkCvKtRsunU
+// ADC - https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html
+
 
 TaskHandle_t ISR = NULL;
 
@@ -39,29 +29,44 @@ void button_task(void* arg)
     vTaskSuspend(NULL); // runs interrupt once
     count++;
     printf("Alert!\n");
-    gpio_set_level(GPIO_OUTPUT_IO_0, count % 2);
+    gpio_set_level(GPIO_NUM_26, count % 2);
   }
 }
 
 void app_main(void)
 {
+    // GPIO
     // initialize Pin 26 for output and turn on
-    esp_rom_gpio_pad_select_gpio(GPIO_OUTPUT_IO_0);
-    gpio_set_direction(GPIO_OUTPUT_IO_0, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+    esp_rom_gpio_pad_select_gpio(GPIO_NUM_26);
+    gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_26, 0);
 
     // initialize Pin 25 for output and turn on
-    esp_rom_gpio_pad_select_gpio(GPIO_OUTPUT_IO_1);
-    gpio_set_direction(GPIO_OUTPUT_IO_1, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_OUTPUT_IO_1, 1);
+    esp_rom_gpio_pad_select_gpio(GPIO_NUM_25);
+    gpio_set_direction(GPIO_NUM_25, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_25, 1);
 
+    // Interrupt
     // Setting up interrupt and GPIO4 for input
-    esp_rom_gpio_pad_select_gpio(GPIO_INPUT_IO_0);
-    gpio_set_direction(GPIO_INPUT_IO_0, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(GPIO_INPUT_IO_0, GPIO_PULLUP_ONLY);
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_NEGEDGE);
+    esp_rom_gpio_pad_select_gpio(GPIO_NUM_4);
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(GPIO_NUM_4, GPIO_PULLUP_ONLY);
+    gpio_set_intr_type(GPIO_NUM_4, GPIO_INTR_NEGEDGE);
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, button_isr_handler, NULL);
+    gpio_isr_handler_add(GPIO_NUM_4, button_isr_handler, NULL);
 
     xTaskCreate(button_task, "button_task", 4096, NULL, 10, &ISR);
+
+
+    // ADC
+    esp_adc_cal_characteristics_t *adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, V_REF, adc_chars);
+    uint32_t reading, voltage;
+    while(1)
+    {
+      vTaskDelay(100/portTICK_RATE_MS); // read delay
+      reading = adc1_get_raw(ADC1_CHANNEL_6);
+      voltage = esp_adc_cal_raw_to_voltage(reading, adc_chars);
+      printf("%d mV\n", voltage);
+    }
 }
