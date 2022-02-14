0357424d1ee5d000a22924031ab14cdc3728fbd9
devshah00 <54751446+devshah00@users.noreply.github.com>
2022-02-14 13:06:25 -0500

    Shows concurrent tasks

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -9,6 +9,10 @@
 #define ESP_INTR_FLAG_DEFAULT 0
 #define V_REF 1100
 
+// Steps if idf.py not found
+// run ". export.sh" (Mac)
+// then go to project and build
+
 
 // Interrupt - https://www.youtube.com/watch?v=VkCvKtRsunU
 // ADC - https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html
@@ -28,8 +32,33 @@ void button_task(void* arg)
   {
     vTaskSuspend(NULL); // runs interrupt once
     count++;
-    printf("Alert!Alert!Alert!Alert!Alert!Alert!Alert!Alert!Alert!\n");
+    printf("Alert!\n");
     gpio_set_level(GPIO_NUM_26, count % 2);
+  }
+}
+
+void blink1Sec(void* arg)
+{
+  bool status = false;
+  while(1)
+  {
+    vTaskDelay(1000/portTICK_RATE_MS);
+    gpio_set_level(GPIO_NUM_25, status);
+    status = !status;
+  }
+}
+
+void readPulse(void* arg)
+{
+  esp_adc_cal_characteristics_t *adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, V_REF, adc_chars);
+  uint32_t reading, voltage;
+  while(1)
+  {
+    vTaskDelay(200/portTICK_RATE_MS); // read delay
+    reading = adc1_get_raw(ADC1_CHANNEL_6);
+    voltage = esp_adc_cal_raw_to_voltage(reading, adc_chars);
+    printf("%d mV\n", voltage);
   }
 }
 
@@ -37,36 +66,25 @@ void app_main(void)
 {
     // GPIO
     // initialize Pin 26 for output and turn on
-    gpio_pad_select_gpio(GPIO_NUM_26);
+    esp_rom_gpio_pad_select_gpio(GPIO_NUM_26);
     gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT);
     gpio_set_level(GPIO_NUM_26, 0);
 
     // initialize Pin 25 for output and turn on
-    gpio_pad_select_gpio(GPIO_NUM_25);
+    esp_rom_gpio_pad_select_gpio(GPIO_NUM_25);
     gpio_set_direction(GPIO_NUM_25, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_25, 1);
+    xTaskCreate(blink1Sec, "blink LED", 1024, NULL, 1, NULL); // 1024=size, NULL=param, 1=lowest priority, NULL=handler?
 
     // Interrupt
     // Setting up interrupt and GPIO4 for input
-    gpio_pad_select_gpio(GPIO_NUM_4);
+    esp_rom_gpio_pad_select_gpio(GPIO_NUM_4);
     gpio_set_direction(GPIO_NUM_4, GPIO_MODE_INPUT);
     gpio_set_pull_mode(GPIO_NUM_4, GPIO_PULLUP_ONLY);
     gpio_set_intr_type(GPIO_NUM_4, GPIO_INTR_NEGEDGE);
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     gpio_isr_handler_add(GPIO_NUM_4, button_isr_handler, NULL);
-
     xTaskCreate(button_task, "button_task", 4096, NULL, 10, &ISR);
 
-
     // ADC
-    esp_adc_cal_characteristics_t *adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, V_REF, adc_chars);
-    uint32_t reading, voltage;
-    while(1)
-    {
-      vTaskDelay(100/portTICK_RATE_MS); // read delay
-      reading = adc1_get_raw(ADC1_CHANNEL_6);
-      voltage = esp_adc_cal_raw_to_voltage(reading, adc_chars);
-      printf("%d mV\n", voltage);
-    }
+    xTaskCreate(readPulse, "read pulse values", 2048, NULL, 5, NULL);
 }
