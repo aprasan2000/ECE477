b74e62c5d48ae85d1fcd8a896d68ddfbd3ea4884
RyanD893 <RyanDomanski3@gmail.com>
2022-04-04 14:26:16 -0400

    pssc testing

--- hello_world/build/esp-idf/main/cmake_install.cmake
+++ hello_world/build/esp-idf/main/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/aksha/OneDrive/Akshaj/Documents/GitHub/ECE477/hello_world/main
+# Install script for directory: C:/Code/ECE477/hello_world/main
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
